3e8e84f1bcbbf233be838e5cf783bcfd52ad571f
fabiojjrs <fabiojjrs1@gmail.com>
2026-04-29 02:55:39 +0000

    Inclui movimentos do cavalo com loops aninhados

--- xadrez.c
+++ xadrez.c
@@ -7,7 +7,7 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-    int torre = 1, rainha = 1;
+    int torre = 1, rainha = 1, cavalo = 1;
 
         printf("\n##### XADREZ #####\n\n");
     // Implementação de Movimentação do Bispo
@@ -40,6 +40,15 @@ int main() {
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+    printf("\n-----CAVALO-----\n");
+    while (cavalo--)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            printf("Baixo\n");
+        }   
+        printf("Esquerda\n");
+    }
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
